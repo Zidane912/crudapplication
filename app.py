8db69db4b77864a709f003ef067b7a3e6bb1f558
Zidane912 <96538941+Zidane912@users.noreply.github.com>
2022-04-12 10:18:48 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from application import app, db
+from os import getenv
+
+if __name__ == "__main__":
+    # Recreate the table schema in the database if environment variable
+    # CREATE_SCHEMA is set to "true". Any other value will not recreate
+    # the schema.
+    #
+    # To set the CREATE_SCHEMA value on Linux, run:
+    #   export CREATE_SCHEMA=true
+    
+    if getenv("CREATE_SCHEMA") != None:
+        if getenv("CREATE_SCHEMA").lower() == "true":
+            db.drop_all()
+            db.create_all()
+            
+    app.run(debug=True, host="0.0.0.0", port=5000)
