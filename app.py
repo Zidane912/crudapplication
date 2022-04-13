be190e6ffedf921196cfc2d2885c49990422cd51
Zidane Alam <alam.zidane912@gmail.com>
2022-04-13 09:30:17 +0000

    made form for application

--- app.py
+++ app.py
@@ -2,12 +2,6 @@ from application import app, db
 from os import getenv
 
 if __name__ == "__main__":
-    # Recreate the table schema in the database if environment variable
-    # CREATE_SCHEMA is set to "true". Any other value will not recreate
-    # the schema.
-    #
-    # To set the CREATE_SCHEMA value on Linux, run:
-    #   export CREATE_SCHEMA=true
 
     if getenv("CREATE_SCHEMA") != None:
         if getenv("CREATE_SCHEMA").lower() == "true":
--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
+import uuid
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URI")
+app.config['SECRET_KEY'] = str(uuid.uuid4) # this was used because wtfforms uses CSRF to protect against unwanted forms being submitted
+# this was done in this way so an environment varibale does not have to be used
 
 db = SQLAlchemy(app)
 
